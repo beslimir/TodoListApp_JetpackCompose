adb2468fbaafe55cffe4935ec33d7751b5c6c3bc
beslimir <marin.beslo@gmail.com>
2022-01-10 14:48:01 +0100

    add AddEditTodo stuff

--- app/src/main/java/com/example/todolistapp_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp_jetpackcompose/MainActivity.kt
@@ -3,19 +3,49 @@ package com.example.todolistapp_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.todolistapp_jetpackcompose.ui.add_edit_todo.AddEditTodoScreen
 import com.example.todolistapp_jetpackcompose.ui.theme.TodoListApp_JetpackComposeTheme
+import com.example.todolistapp_jetpackcompose.ui.todo_list.TodoListScreen
+import com.example.todolistapp_jetpackcompose.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoListApp_JetpackComposeTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
             }
         }
     }
