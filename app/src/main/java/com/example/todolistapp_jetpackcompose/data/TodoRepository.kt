daf8e50c675a43b426e3e899a8d8f480bf42ebdc
beslimir <marin.beslo@gmail.com>
2022-01-10 10:35:21 +0100

    Add Dao, Database, Repository

--- app/src/main/java/com/example/todolistapp_jetpackcompose/data/TodoRepository.kt
+++ app/src/main/java/com/example/todolistapp_jetpackcompose/data/TodoRepository.kt
@@ -1,21 +1,15 @@
 package com.example.todolistapp_jetpackcompose.data
 
-import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
-@Dao
-interface TodoDao {
+interface TodoRepository {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTodo(todo: Todo)
 
-    @Delete
     suspend fun deleteTodo(todo: Todo)
 
-    @Query("SELECT * FROM Todo WHERE id = :id")
     suspend fun getTodoById(id: Int): Todo?
 
-    @Query("SELECT * FROM Todo")
     fun getTodos(): Flow<List<Todo>>
 
 }
